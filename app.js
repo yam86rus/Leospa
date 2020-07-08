d5416859b7023ca869e051c4d60c69f57ff1ce1b
Maxim <64529642+yam86rus@users.noreply.github.com>
2020-07-08 11:24:18 +0500

    up

---
+++ app.js
@@ -0,0 +1,36 @@
+$(function () {
+    // === Fixed Header ===
+    let header = $('#header');
+    let about = $('#about');
+    let aboutH = about.innerHeight();
+    let scrollPos = $(window).scrollTop();
+
+    $(window).on('scroll load resize', function () {
+        aboutH = about.innerHeight();
+        scrollPos = $(this).scrollTop();
+        if (scrollPos > aboutH) {
+            header.addClass('fixed');
+
+        } else {
+
+            header.removeClass('fixed');
+        }
+
+    });
+    // === / Fixed Header
+
+    // === Smooth scroll ===
+    $('[data-scroll]').on('click', function (event) {
+        event.preventDefault();
+
+        let elementId = $(this).data('scroll');
+        let elementOffset = $(elementId).offset().top;
+        console.log(elementOffset);
+
+        $('html, body').animate({
+            scrollTop: elementOffset - 90
+        }, 700);
+    });
+    // === / Smooth scroll ===
+
+});
